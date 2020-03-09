dbf19dc8942066be21370d38105bda05a64045c2
Adrian Barros <adriansbarros1@gmail.com>
2020-03-09 01:04:34 -0400

    heruku

--- ttp_fullstack/api/app.js
+++ ttp_fullstack/api/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
+
 
 const stocks = require('./routes/api_routes/Stocks');
 //body parser middleware
@@ -18,4 +19,13 @@ mongoose
     .catch(err => console.log(err));
 //Use Routes
 app.use('/api/stocks', stocks)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'src', 'build', 'index.html'));
+    });
+}
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));

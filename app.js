c20d1691728e03e9eb462bc2ba5af3806e373f65
Adrian Barros <adriansbarros1@gmail.com>
2020-03-09 20:15:30 -0400

    quotes

--- app.js
+++ app.js
@@ -30,12 +30,13 @@ app.use("/api/users", require("./routes/api_routes/Users"));
 app.use("/api/auth", require("./routes/api_routes/auth"));
 
 
-if (process.env.NODE_ENV === `production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static("/client/build"));
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server started on port ${ PORT } `));
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
